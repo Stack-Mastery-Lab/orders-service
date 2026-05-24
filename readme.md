bd03163a99ea5ad07af8060418d13a29d4c786bb
JhonLlanes <jlc_rb@live.com>
2026-05-24 10:21:53 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Orders Service
+## 
+
+``` shell
+ docker run -d --name postgres-orders -p 5432:5432 -e POSTGRES_DB=db_orders -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin postgres:16
+```
---
+++ src/test/java/com/relatandopapel/ordersservice/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relatandopapel.ordersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
